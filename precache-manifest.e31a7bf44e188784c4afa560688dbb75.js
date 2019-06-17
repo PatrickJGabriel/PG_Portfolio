a721a98adfc344a95692c6eb51ec393d11920cad
PatrickJGabriel <patrickgabrieldesigns@gmail.com>
2019-06-17 14:54:26 -0400

    Updates

---
+++ precache-manifest.e31a7bf44e188784c4afa560688dbb75.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5cb50745d0c046552c1",
+    "url": "/static/css/main.5bdd9be0.chunk.css"
+  },
+  {
+    "revision": "e5cb50745d0c046552c1",
+    "url": "/static/js/main.96adc697.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a750c6a824684b61f4e0",
+    "url": "/static/css/2.342ccf8b.chunk.css"
+  },
+  {
+    "revision": "a750c6a824684b61f4e0",
+    "url": "/static/js/2.b25afd5f.chunk.js"
+  },
+  {
+    "revision": "0ca9ec89d378098714d069776b542948",
+    "url": "/static/media/amps_portfolio2.0ca9ec89.png"
+  },
+  {
+    "revision": "97428580d25968104161d38e79a48367",
+    "url": "/static/media/zravia_portfolio2.97428580.png"
+  },
+  {
+    "revision": "72e8c01fdd7c8c41f6192707a0c2a1cc",
+    "url": "/static/media/scrim_portfolio2.72e8c01f.png"
+  },
+  {
+    "revision": "7e26561e96ef6e2ccc7e61ad0edeeb9e",
+    "url": "/static/media/encore_portfolio2.7e26561e.png"
+  },
+  {
+    "revision": "4964e5af38de69391734e4eb724b25a7",
+    "url": "/index.html"
+  }
+];
